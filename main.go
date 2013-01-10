22b208a5325c917a89c7dc36236f8cc87130420b
Lukasz Gruner <ukaszg@gmail.com>
2013-01-10 04:29:33 +0100

    gofmt main

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"./status"
 	"fmt"
@@ -8,10 +7,10 @@ import (
 )
 
 var (
-	cpu     = status.NewCpu(1 * time.Second)
-	mem     = status.NewMem()
-	lavg    = status.NewLoadavg()
-	bat     = status.NewBattery("/sys/class/power_supply/BAT1/", false)
+	cpu  = status.NewCpu(1 * time.Second)
+	mem  = status.NewMem()
+	lavg = status.NewLoadavg()
+	bat  = status.NewBattery("/sys/class/power_supply/BAT1/", false)
 )
 
 func Time() string {
@@ -19,13 +18,13 @@ func Time() string {
 }
 
 func main() {
-	c  := cpu.Get()
+	c := cpu.Get()
 	cs := status.Bar(16, c)
-	m  := mem.Get()
+	m := mem.Get()
 	ms := status.Bar(16, m)
-	l  := lavg.Get()
-	t  := Time()
-	b  := bat.Get()
+	l := lavg.Get()
+	t := Time()
+	b := bat.Get()
 
 	fmt.Printf("mem: %s %.1f%% | cpu: %s %.1f%%  %s | bat: %.0f%% | %s\n", ms, m, cs, c, l, b, t)
 }
